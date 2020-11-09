eba274c80843f10c8f2f5b70bfc5a9269a163046
Tim Quelch <tim@tquelch.com>
2020-11-09 17:21:10 +1100

    Fix citeproc heading level

--- .dir-locals.el
+++ .dir-locals.el
@@ -6,6 +6,24 @@
                         (require 'ox-hugo)
                         (require 'citeproc-org)
                         (with-eval-after-load 'citeproc-org
-                          (citeproc-org-setup))))
+                          (citeproc-org-setup)
+                          (setq citeproc-org-org-bib-header "** References\n")
+
+                          (defun tq/min-headline-level ()
+                            (--> (org-element-parse-buffer)
+                                 (org-element-map it 'headline (apply-partially #'org-element-property :level))
+                                 (or it '(0))
+                                 (-min it)))
+
+                          (defun tq/citeproc-org-render-references-advice (orig args)
+                            (let* ((minlevel (tq/min-headline-level))
+                                   (totallevel (max 1 minlevel))
+                                   (citeproc-org-org-bib-header
+                                    (concat (make-string totallevel ?*)
+                                            (string-trim-left citeproc-org-org-bib-header "\\**"))))
+                              (apply orig args)))
+
+                          (advice-add 'cite-proc-org-render-references :around
+                                      #'tq/citeproc-org-render-references-advice))))
               (org-ref-default-bibliography . ("static/references.bib"))
               (eval . (add-to-list 'org-hugo-external-file-extensions-allowed-for-copying "asc")))))
