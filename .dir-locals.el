df9b4b31efaad57b8b3b08d4835865a3e1df33d5
Tim Quelch <tim@tquelch.com>
2020-11-09 18:26:28 +1100

    Move build config to minor mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,28 +2,7 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((org-mode . ((eval . (progn
-                        (require 'org-id)
-                        (require 'ox-hugo)
-                        (require 'citeproc-org)
-                        (with-eval-after-load 'citeproc-org
-                          (citeproc-org-setup)
-                          (setq citeproc-org-org-bib-header "** References\n")
-
-                          (defun tq/min-headline-level ()
-                            (--> (org-element-parse-buffer)
-                                 (org-element-map it 'headline (apply-partially #'org-element-property :level))
-                                 (or it '(0))
-                                 (-min it)))
-
-                          (defun tq/citeproc-org-render-references-advice (orig args)
-                            (let* ((minlevel (tq/min-headline-level))
-                                   (totallevel (max 1 minlevel))
-                                   (citeproc-org-org-bib-header
-                                    (concat (make-string totallevel ?*)
-                                            (string-trim-left citeproc-org-org-bib-header "\\**"))))
-                              (apply orig args)))
-
-                          (advice-add 'cite-proc-org-render-references :around
-                                      #'tq/citeproc-org-render-references-advice))))
-              (org-ref-default-bibliography . ("build/references.bib"))
-              (eval . (add-to-list 'org-hugo-external-file-extensions-allowed-for-copying "asc")))))
+                        (load-file (expand-file-name
+                                    "tquelch-dev.el"
+                                    (file-name-directory (buffer-file-name))))
+                        (tquelch-dev-mode))))))
---
+++ tquelch-dev.el
@@ -0,0 +1,28 @@
+(require 'org-id)
+(require 'ox-hugo)
+(require 'citeproc-org)
+
+(defun tqdev/min-headline-level ()
+  (--> (org-element-parse-buffer)
+       (org-element-map it 'headline (apply-partially #'org-element-property :level))
+       (or it '(0))
+       (-min it)))
+
+(defun tqdev/citeproc-org-render-references-advice (orig args)
+  (let* ((minlevel (tqdev/min-headline-level))
+         (totallevel (max 1 minlevel))
+         (citeproc-org-org-bib-header
+          (concat (make-string totallevel ?*)
+                  (string-trim-left citeproc-org-org-bib-header "\\**"))))
+    (apply orig args)))
+
+(define-minor-mode tquelch-dev-mode
+  "Minor mode for developing my website"
+  :lighter " tqdev"
+  (when tquelch-dev-mode
+    (citeproc-org-setup)
+    (setq-local citeproc-org-org-bib-header "* References\n")
+    (advice-add 'cite-proc-org-render-references :around
+                #'tqdev/citeproc-org-render-references-advice)
+    (setq-local org-ref-default-bibliography '("build/references.bib"))
+    (add-to-list 'org-hugo-external-file-extensions-allowed-for-copying "asc")))
